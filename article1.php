6e7b8cc98d4bba0a2432702f893ec5834efd81ff
Aron Bencsik <101459795+aronbencsiknu@users.noreply.github.com>
2024-10-15 12:07:47 +0100

    updating news section

---
+++ article1.php
@@ -0,0 +1,70 @@
+<?php include 'header.php'; ?>
+    <main>
+    <h2>Hatszáz milliós biotechnológiai beruházás a hátrányos helyzetű Tarnabodon</h2>
+    <p>Szécsény, Tarnabod, 2024.10.15.</p>
+    
+
+    <p><b>Magyarországon egyedülálló biotechnológiai üzem épül a Heves vármegyei Tarnabodon. A beruházás célja a világ egyik leghatékonyabb antioxidánsának, az algákból kinyert astaxanthin előállítása, amely alapja lesz egy új és innovatív funkcionális italnak.</b></p>
+    <img src="images/article3.jpg" alt="">
+    <p>A több szakmai elismeréssel is díjazott Funky Forest® 100%-os préslé és smoothie termékcsalád gyártója, a Házikó Farm Kft. sikeresen pályázott 2023-ban az Élelmiszeripari Beszállító-fejlesztési Program (ÉLIP BP) keretében, hogy technológiai fejlesztéseket valósítson meg egy innovatív ötlet megvalósítása érdekében. A projekt célja egy egészséges alternatívát nyújtó funkcionális ital kifejlesztése és piaci bevezetése, amelynek hatóanyaga az algából kinyert astaxanthin.
+<p>Az astaxanthin egy rendkívül erős antioxidáns, amely többek között algák, élesztők, lazacok és rákfélék által termelt vörös pigment. Kiemelkedő biológiai tulajdonságai közé tartozik, hogy antioxidáns kapacitása jelentősen meghaladja a C-vitamin és az E-vitamin hatékonyságát. Az astaxanthin képes semlegesíteni a szabad gyököket, amelyek károsítják a sejteket és hozzájárulnak különböző betegségek kialakulásához. 
+<p>A több mint hatszáz milliós beruházás egyrészt a Házikó Farm Kft. Szécsényben lévő telephelyén, másrészt - a technológiai beszállítóként kiválasztott Glia Kft. közreműködésével - a Heves vármegyei Tarnabodon valósul meg. 
+<p>A Glia Kft. több mint két évtizedes tapasztalattal rendelkezik a biotechnológiai kutatás-fejlesztés területén, és számos sikeres projektet valósított meg a mezőgazdasági és ipari szektorban. A cég kiemelkedő szakértelemmel bír az innovatív technológiák, köztük az algaalapú biotechnológiai megoldások fejlesztésében. 
+<p>A projekt technológiai beszállítójaként a fotobioreaktorok gyártásáért, leszállításáért és üzembe helyezéséért felel. A Tarnabodi Kísérleti Biotechnológiai Üzem a meglévő és épülő ipari infrastruktúrának, valamint az új, fenntartható technológiáknak köszönhetően lehetőséget biztosít az új technológia kialakítására.  
+<p>A két Társaság működése hasonló értékrendszeren alapul, mindkét vállalat kiemelten törekszik a fenntarthatóság, az innováció és a társadalmi felelősségvállalás előmozdítására. A Házikó Farm Kft. a helyi alapanyagok felhasználásával, valamint a környezettudatos gyártási módszerekkel élen jár a fenntartható élelmiszeriparban. Hasonlóan, a Glia Kft. is elkötelezett a fenntartható biotechnológiai újítások mellett. 
+<p>A két vállalat együttműködése nemcsak technológiai előrelépést jelent, hanem hozzájárul a hátrányos helyzetű települések fejlesztéséhez is.
+</p>
+
+    <h3>A Házikó Farm Kft.-ről</h3>
+    <p>A Házikó Farm Kft. 2020-ban fejezte be saját gyümölcsfeldolgozó üzemének építését Szécsényben. A helyszínválasztás tudatos döntés volt, mivel a vállalat számára kiemelten fontos, hogy alapanyagai helyi, nyomon követhető forrásból származzanak, valamint hogy hozzájáruljanak egy hátrányos helyzetű térség fejlődéséhez és a szakképzett munkaerő helyben tartásához.
+  <p>A Házikó Farm Kft. egyedi ízkombinációit úgy alakította ki, hogy az alapanyagok nagy részét a régió termelői biztosíthassák. A vállalat elkötelezett a fenntarthatóság és az ökológiai lábnyom minimalizálása mellett, így a gyártás során keletkező maradékokat a környező állatfarmok számára takarmányként hasznosítják. Egy regeneráló projekt keretében a préselt ribizli szárazanyag maradéka komposzt anyagként és talaj-építőként él tovább, hatalmas tápanyag utánpótlást biztosítva a következő generációs terményeknek.
+  <p>A Házikó Farm Kft. célja, hogy nagyrészt helyi alapanyagokból készült, természetes élelmiszereket állítson elő, különös figyelmet fordítva a természeti erőforrások megóvására. A vállalat a Funky Forest® termékcsalád révén vált ismertté. A Funky Forest® termékek több szakmai elismerést is kaptak, köztük az Év Innovatív Terméke díjat, ami a vállalat fenntarthatóság és innováció iránti elkötelezettségét jelzi. A Házikó Farm Kft. szoros együttműködést ápol a helyi termelőkkel, és kiemelten törekszik a társadalmi és környezeti fenntarthatóság előmozdítására..</p>
+
+    <h3>A Glia Kft.-ről</h3>
+    <p>A Glia Kutatás-Fejlesztési Innovációs Kft., mely a Glia Nova Zrt. egyik leányvállalata 2003 óta foglalkozik innovációs projektek megvalósításával, kiemelten a biotechnológia, a mezőgazdaság és az agrárinformatika területén. A vállalat számos mezőgazdasági és infrastrukturális fejlesztést valósít meg, különös figyelmet fordítva a fenntarthatóságra és a környezetvédelemre. Az egyik legkiemelkedőbb fejlesztési területük az alga-baktérium ko-kultivációs technológia, amely az algák és baktériumok együttes alkalmazását integrálja a mezőgazdasági és kommunális szennyvízkezelési folyamatokban. Ez az innovatív módszer nemcsak a környezeti fenntarthatóságot támogatja, hanem értékes biológiai termékeket is előállít.
+<p>A Glia Nova Zrt. nemrégiben szerezte meg a biotechnológiai innovációban élenjáró Albitech Kft. többségi részesedését, amely a mezőgazdasági célú algatermelés egyik vezető képviselője Magyarországon.
+<p>Tarnabodon a Felzárkózó Települések Program részeként a vállalat hosszú távú partnerként működik együtt a Máltai Szeretetszolgálattal. A Tarnabod 2.0  a Jövőgyár projekt keretében a Glia Kft. technológiai fejlesztései segítik a helyi közösség gazdasági felzárkóztatását, fenntartható gazdasági modellek és környezetbarát gyakorlatok kialakításával. Ezek az innovatív megoldások más településeken is alkalmazhatók, hozzájárulva a vidéki szegénység csökkentéséhez és a fenntartható vidékfejlesztéshez.</p>
+
+    <h3>Az algából nyert astaxanthinról</h3>
+    <p>Az astaxanthin egy rendkívül értékes vegyület, amely a karotinoidok xantofil családjába tartozik, és természetes antioxidánsként kiemelkedő szerepet játszik. Az astaxanthint elsősorban a Haematococcus pluvialis mikroalgából nyerik, amely különleges biotechnológiai eljárások segítségével nagy mennyiségű astaxanthint halmoz fel stressz hatására. Ez az anyag különleges tulajdonságai miatt széles körben alkalmazható,  nemcsak az élelmiszer- és takarmányipar, hanem a kozmetikai és gyógyszeripban.
+<p>Az astaxanthin az emberi szervezetben antioxidáns hatásával védi a sejteket a szabad gyökök káros hatásaitól, ezáltal lassítva az öregedési folyamatokat és támogatva az immunrendszert. Tudományos kutatások szerint az astaxanthin jelentős szerepet játszik a bőr védelmében az UV-sugárzás okozta károsodásokkal szemben, továbbá sikeresen alkalmazható különböző neurológiai betegségek (például az Alzheimer- és Parkinson-kór) kezelésében is.
+<p>Az astaxanthinról továbbá ismert az is, hogy fokozza a sportteljesítményt, csökkenti az izmok helyreállítási idejét, és hozzájárul az állóképesség növeléséhez, így természetes szuper élelmiszerként is ismertté vált. </p>
+
+   
+    <table>
+        <tr>
+            <th>Házikó Farm Kft.</th>
+            <th>Glia Kft.</th>
+        </tr>
+        <tr>
+            <td>Bognár Botond</td>
+            <td>Bencsik Attila</td>
+        </tr>
+        <tr>
+            <td>Ügyvezető</td>
+            <td>Ügyvezető igazgató</td>
+        </tr>
+        <tr>
+            <td>Telefonszám: +36 30 758 0417</td>
+            <td>Telefonszám: +36 20 929 4412</td>
+        </tr>
+        <tr>
+            <td>Email: <a href="mailto:info@haziko.farm">info@haziko.farm</a></td>
+            <td>Email: <a href="mailto:attila@glia.hu">attila@glia.hu</a></td>
+        </tr>
+        <tr>
+            <td><a href="https://haziko.farm/" target="_blank">https://haziko.farm/</a></td>
+            <td><a href="https://www.glia.hu/" target="_blank">https://www.glia.hu/</a></td>
+        </tr>
+        <tr>
+            <td><a href="https://funkyforest.hu/" target="_blank">https://funkyforest.hu/</a></td>
+            <td><a href="https://www.glia.hu/tarnabod/" target="_blank">https://www.glia.hu/tarnabod/</a></td>
+        </tr>
+    </table>
+    
+    
+    
+    
+    
+</main>
+<?php include 'footer.php'; ?>
--- news.php
+++ news.php
@@ -6,36 +6,48 @@
         <!-- News Item 1 -->
         <a href="article1.php" class="news-item" style="background-image: url('images/article1.jpg');">
             <div class="news-overlay">
-                <h2>Article Headline 1</h2>
+                <p>2024.10.15.</p>
+                <hr>
+                <h2>Biotechnológiai beruházás Tarnabodon</h2>
             </div>
         </a>
         <!-- News Item 2 -->
         <a href="article2.php" class="news-item" style="background-image: url('images/article2.jpg');">
             <div class="news-overlay">
+            <p>2024.10.15.</p>
+            <hr>
                 <h2>Article Headline 2</h2>
             </div>
         </a>
         <!-- News Item 3 -->
         <a href="article3.php" class="news-item" style="background-image: url('images/article3.jpg');">
             <div class="news-overlay">
+            <p>2024.10.15.</p>
+            <hr>
                 <h2>Article Headline 3</h2>
             </div>
         </a>
         <!-- News Item 1 -->
         <a href="article1.php" class="news-item" style="background-image: url('images/article1.jpg');">
             <div class="news-overlay">
+            <p>2024.10.15.</p>
+            <hr>
                 <h2>Article Headline 1</h2>
             </div>
         </a>
         <!-- News Item 2 -->
         <a href="article2.php" class="news-item" style="background-image: url('images/article2.jpg');">
             <div class="news-overlay">
+            <p>2024.10.15.</p>
+            <hr>
                 <h2>Article Headline 2</h2>
             </div>
         </a>
         <!-- News Item 3 -->
         <a href="article3.php" class="news-item" style="background-image: url('images/article3.jpg');">
             <div class="news-overlay">
+            <p>2024.10.15.</p>
+            <hr>
                 <h2>Article Headline 3</h2>
             </div>
         </a>
