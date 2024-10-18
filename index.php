90d8a26a85db4a37ab0570d9c8a8e5b1ae396f7b
Aron Bencsik <101459795+aronbencsiknu@users.noreply.github.com>
2024-10-18 12:22:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -24,15 +24,18 @@
         <!-- News Item 1 -->
         <a href="article1.php" class="news-item" style="background-image: url('images/article1.jpg');">
             <div class="news-overlay">
-                  <p style="font-size: 14px; color: #fff;">2024-10-16</p> 
+                  <p>2024-10-16</p> 
+                  <hr>
                 <h2>Biotechnológiai beruházás Tarnabodon</h2>
             </div>
         </a>
         <!-- News Item 2 -->
         <a href="article2.php" class="news-item" style="background-image: url('images/article2.jpg');">
             <div class="news-overlay">
+            <p>2024-10-16</p>
+            <hr>
                 <h2> Albitech Kft. Apport</h2>
-                    <p style="font-size: 14px; color: #fff;">2024-10-16</p>
+                    
             </div>
         </a>
     </div>        
